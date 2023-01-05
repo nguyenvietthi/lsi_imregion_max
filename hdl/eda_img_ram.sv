277e5123b47a9638d98ad6a07d3b6d7994b75b1c
thi <nguyenvietthi.ng@gmail.com>
2023-01-05 23:20:11 +0700

    add eda_img_ram.sv

--- hdl/eda_img_ram.sv
+++ hdl/eda_img_ram.sv
@@ -4,6 +4,8 @@ module eda_img_ram #(
   parameter PIXEL_WIDTH      = 8                              ,
   parameter WINDOW_WIDTH     = 9                              ,
   parameter ADDR_WIDTH       = $clog2(M*N)                    ,
+  parameter I_WIDTH          = $clog2(M)                      ,
+  parameter J_WIDTH          = $clog2(M)                      ,
   parameter NEIGH_ADDR_WIDTH = ADDR_WIDTH * (WINDOW_WIDTH - 1)
 )(
   input                                           clk             ,
@@ -11,13 +13,13 @@ module eda_img_ram #(
   input                                           write_en        ,
   input        [ADDR_WIDTH - 1:0]                 wr_addr         ,
   input        [PIXEL_WIDTH- 1:0]                 pixel_in        ,
-  input        [ADDR_WIDTH - 1:0]                 center_addr     ,
+  input        [ADDR_WIDTH - 1:0]                 center_addr     , //{i, j}
   output       [PIXEL_WIDTH * WINDOW_WIDTH - 1:0] window_values   ,
   output logic [WINDOW_WIDTH - 2:0]               neigh_addr_valid,
   output       [NEIGH_ADDR_WIDTH - 1:0]           neigh_addr       
 );
 
-  logic [PIXEL_WIDTH - 1:0] img_memory [ADDR_WIDTH - 1:0];
+  logic [PIXEL_WIDTH - 1:0] img_memory [I_WIDTH - 1:0] [J_WIDTH - 1:0];
 
   //|----------|--------|-----------|
   //| upleft   |   up   | upright   |
@@ -37,33 +39,62 @@ module eda_img_ram #(
   logic downright_addr;
 
   // Write pixel into memory
+  logic [I_WIDTH - 1] i_pixel;
+  logic [J_WIDTH - 1] j_pixel;
+
+  assign i_pixel = wr_addr[I_WIDTH - 1:J_WIDTH];
+  assign j_pixel = wr_addr[J_WIDTH - 1:0]      ;
+
   always_ff @(posedge clk or negedge reset_n) begin
     if(write_en) begin 
-      img_memory[wr_addr] <= pixel_in;
+      img_memory[i_pixel][j_pixel] <= pixel_in;
     end
   end
 
   // Generate neighborhood address of center address
-  assign upleft_addr    = center_addr - M - 1;
-  assign up_addr        = center_addr - M    ;
-  assign upright_addr   = center_addr - M + 1; 
-  assign left_addr      = center_addr - 1    ;
-  assign right_addr     = center_addr     + 1;
-  assign downleft_addr  = center_addr + M - 1; 
-  assign down_addr      = center_addr + M    ;
-  assign downright_addr = center_addr + M + 1;
+  logic [I_WIDTH - 1] i_center;
+  logic [J_WIDTH - 1] j_center;
+
+  assign i_center = center_addr[I_WIDTH - 1:J_WIDTH];
+  assign j_center = center_addr[J_WIDTH - 1:0]      ;
+
+  assign upleft_addr    = {i_center - 1, j_center - 1};
+  assign up_addr        = {i_center - 1, j_center    };
+  assign upright_addr   = {i_center - 1, j_center + 1};
+  assign left_addr      = {i_center    , j_center - 1};
+  assign right_addr     = {i_center    , j_center + 1};
+  assign downleft_addr  = {i_center + 1, j_center - 1};
+  assign down_addr      = {i_center + 1, j_center    };
+  assign downright_addr = {i_center + 1, j_center + 1};
 
   assign neigh_addr = {upleft_addr, up_addr, upright_addr, left_addr, right_addr, downleft_addr, down_addr, downright_addr};
 
   // Assign window values
-  assign window_values = {img_memory[upleft_addr], img_memory[up_addr], img_memory[upright_addr],
-                          img_memory[left_addr], img_memory[center_addr], img_memory[right_addr], 
-                          img_memory[downleft_addr], img_memory[down_addr], img_memory[downright_addr]};
+  assign window_values = {img_memory[i_center - 1][j_center - 1], img_memory[i_center - 1][j_center    ], img_memory[i_center - 1][j_center + 1], 
+                          img_memory[i_center    ][j_center - 1], img_memory[i_center    ][j_center ], img_memory[i_center    ][j_center + 1], 
+                          img_memory[i_center + 1][j_center - 1], img_memory[i_center + 1][j_center    ], img_memory[i_center + 1][j_center + 1]}
 
   // Generate neighborhood address valid
 
-
-
-
+  always_comb begin
+    if(i_center == 0) begin 
+      if(j_center == 0) begin 
+        neigh_addr_valid = 8'b00001011;
+      end else if(j_center == M - 1) begin 
+        neigh_addr_valid = 8'b00010110;
+      end else begin 
+        neigh_addr_valid = 8'b00011111;
+      end
+    end else if(i_center == N - 1) begin 
+      if(j_center == 0) begin 
+        neigh_addr_valid = 8'b01101000;
+      end else if(j_center == M - 1) begin 
+        neigh_addr_valid = 8'b11010000;
+      end else begin 
+        neigh_addr_valid = 8'b11111000;
+      end
+    end
+  
+  end
 
 endmodule
