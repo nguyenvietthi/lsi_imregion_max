da876a41f3324b3c8a8dfb8a0da2ddc9dbee3a05
nguyenducquang1212 <quangnguyenduc1212@gmail.com>
2023-01-08 22:07:45 +0700

    add eda_fifo and eda_output_ram

---
+++ hdl/eda_output_ram.sv
@@ -0,0 +1,48 @@
+`include "eda_global_define.vh"
+
+module eda_output_ram #(
+  parameter M                = CFG_M                            ,
+  parameter N                = CFG_N                            ,
+  parameter PIXEL_WIDTH      = CFG_PIXEL_WIDTH                  ,
+  parameter WINDOW_WIDTH     = CFG_WINDOW_WIDTH                 ,
+  parameter ADDR_WIDTH       = CFG_ADDR_WIDTH                   ,
+  parameter I_WIDTH          = CFG_I_WIDTH                      ,
+  parameter J_WIDTH          = CFG_J_WIDTH                      
+)(
+  input                   clk           ,    // Clock
+  input                   reset_n       ,  // Asynchronous reset active low
+  input                   clear         ,  // Asynchronous reset active low
+  input                   update_output ,
+  input                   compare_out   ,
+  input        [M-1][N-1] strb_value    ,
+  output logic [M-1][N-1] matrix_output 
+);
+
+
+genvar i, j;
+
+generate
+  for (i = 0; i < M; i = i+1) begin
+    for (j = 0; j < N; j = j+1) begin
+      always_ff @(posedge clk or negedge reset_n) begin
+        if(~reset_n) begin
+          matrix_output[i][j] <= 1'b1;
+        end else begin
+          // clear matrix, using when load new image
+          if (clear) begin
+            matrix_output[i][j] <= 1'b1;
+          end
+          // Update pixels which has strb
+          else if (!compare_out) begin
+            if (strb_value[i][j]) begin
+              matrix_output[i][j] <= 1'b0;
+            end
+          end
+        end
+      end
+    end
+  end
+endgenerate
+
+
+endmodule : eda_output_ram
